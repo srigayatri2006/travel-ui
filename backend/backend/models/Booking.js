6cf388fe8c8404ee407af933a33840dcb0be014e
srigayatri2006 <srigayatri0304.com>
2026-06-16 14:00:25 +0530

    Initial commit

---
+++ backend/backend/models/Booking.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+    },
+
+    destination: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Booking", bookingSchema);
